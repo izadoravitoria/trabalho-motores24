e78eb7b8ae6428029a5ae0b11a92665770c78f91
izadoravitoria <izadoravitoriag@gmail.com>
2024-08-26 11:53:45 -0300

    fase 1

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
-    public Rigidbody rg
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("start") ;
+        Debug.Log("start");
+        TryGetComponent(out rb);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("update");
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        
+        UnityEngine.Vector3 direcao = new Vector3 (x, 0, y);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
     }
 }
