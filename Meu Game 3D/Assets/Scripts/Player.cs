53defe0ccc421b33adf7f66e530f510297dfb161
izadoravitoria <izadoravitoriag@gmail.com>
2024-09-02 11:51:59 -0300

    Trabalho Meu Game 3d

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,28 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour 
 {
     public int velocidade = 10;
     public Rigidbody rb;
+    public int forcaPulo = 10;
+    public bool noChao = true;
 
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("start");
         TryGetComponent(out rb);
-
     }
 
-    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
+    }
+
     void Update()
     {
         Debug.Log("update");
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        
-        UnityEngine.Vector3 direcao = new Vector3 (x, 0, y);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+
+        }
+        if (transform.position.y < -5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
