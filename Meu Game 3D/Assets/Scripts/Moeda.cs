60d0db4f89dbc169b6a9790bb4abc36842a5f455
izadoravitoria <izadoravitoriag@gmail.com>
2024-09-03 10:16:20 -0300

    Meu Game 3D 03

---
+++ Meu Game 3D/Assets/Scripts/Moeda.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player");
+        {
+            Destroy(gameObject);
+        }
+    }
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * velocidadeGiro * Time.deltaTime);
+    
+    }
+}
